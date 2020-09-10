58984f05635d188432c175572e3cdff381dd1b61
xiao970205 <1037426886@qq.com>
2020-09-11 06:09:01 +0800

    创建项目

---
+++ src/main/java/com/Hesher/demo/HesherStudioApplication.java
@@ -0,0 +1,13 @@
+package com.Hesher.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HesherStudioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HesherStudioApplication.class, args);
+	}
+
+}
